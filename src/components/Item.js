965f345eba5ace6ddfa5ec03ec3b5c8437137320
GhaniBahri <bahri.ghanou@gmail.com>
2023-09-11 19:09:49 +0200

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isChecked, setIsChecked]= useState(false)
+
+  function handleChecked(){
+    setIsChecked(()=>!isChecked)
+  }
+
+  const btnColor = isChecked? "remove" : "add"
+  const btnText = isChecked? "Remove from Cart": "Add to Cart"
+  const itemChecked = isChecked? "in-cart": ""
   return (
-    <li className="">
+    <li className={itemChecked}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={btnColor} onClick={handleChecked}>{btnText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [filter, setFilter]= useState("All")
+  function handleFilter(e){
+    setFilter(e.target.value)
+  }
+
+  const filteredItems = items.filter(item=>{
+    if(filter === 'All'){ return true}
+    else{return filter === item.category}
+    
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
